a45636148b8f4fea9441c598d48704aafcd01193
mohaijiang <mohaijiang110@163.com>
2022-12-12 15:57:21 +0800

    rollback

--- app/src/LoadingContainer.js
+++ app/src/LoadingContainer.js
@@ -4,11 +4,9 @@ import { drizzleReactHooks } from "@drizzle/react-plugin";
 const { useDrizzleState } = drizzleReactHooks;
 
 function LoadingContainer({children}) {
+
   const drizzleStatus = useDrizzleState(state => state.drizzleStatus);
 
-  const web3 = useDrizzleState(state => state.web3);
-  console.log(web3)
-  console.log(drizzleStatus)
   if(drizzleStatus.initialized === false) {
     return "Loading ...";
   }
